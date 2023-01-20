6f2accd90991618f42fedba4006f0908f9b5e91c
GarrettA01 <gman.anderson2001@gmail.com>
2023-01-20 10:07:09 -0700

    Added server.js, package-lock, extra db.json test note

---
+++ server.js
@@ -0,0 +1,33 @@
+// require internal and external modules
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notesDb = require("./db/db.json");
+const api = require("./routes/index.js");
+const randoId = require("./helpers/randoId");
+const notesRouter = require("./routes/noteRoutes");
+
+// set up express as object and direct the port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware for parsing json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// points to public directory
+app.use(express.static("public"));
+app.use("/api/notes", notesRouter);
+
+// view html files when paths are accessed
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// listener on port 3001
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
