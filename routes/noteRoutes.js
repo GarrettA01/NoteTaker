7d111eccf95f410fddbb50c2d412f50450285161
GarrettA01 <gman.anderson2001@gmail.com>
2023-01-20 13:51:45 -0700

    Added delete function, added id's to test notes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -16,7 +16,7 @@ api.post("/notes", (req, res) => {
   const createNote = {
     title: req.body.title,
     text: req.body.text,
-    id: randoId,
+    id: randoId(),
   };
 
   //parses db.son again to read new note
@@ -27,6 +27,17 @@ api.post("/notes", (req, res) => {
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
 
   res.json(notes);
+});
+
+api.delete("/notes/:id", (req, res) => {
+  let noteId = req.params.id.toString();
+
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  const newNotes = notes.filter((note) => note.id.toString() !== noteId);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(newNotes));
+  res.json(newNotes);
 });
 
 module.exports = api;
